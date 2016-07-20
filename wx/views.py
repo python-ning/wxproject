a508c4093281f3e0f15bbbe28d364d6adb7d2898
ning.chen@speedycloud.cn <ning.chen@speedycloud.cn>
2016-07-20 16:26:29 -0700

    update

--- wx/views.py
+++ wx/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from models import WechatUserInfo
 import requests
 import json
+from django.core.cache import cache
 
 APPID = "wx2e4b3f5d864ec373"
 SECRET = "34420c2496cbb60d75e02e1ef1bb8183"
@@ -37,5 +38,33 @@ def index(request):
         'province': paramss['province'],
         'country': paramss['country'],
         'headimgurl': paramss['headimgurl']
+    }
+    cache.set("openid", paramss['openid'], 2 * 3600)
+    cache.set("nickname", paramss['nickname'], 2 * 3600)
+    cache.set("sex", SEX[paramss['sex']], 2 * 3600)
+    cache.set("language", paramss['language'], 2 * 3600)
+    cache.set("city", paramss['city'], 2 * 3600)
+    cache.set("province", paramss['province'], 2 * 3600)
+    cache.set("country", paramss['country'], 2 * 3600)
+    cache.set("headimgurl", paramss['headimgurl'], 2 * 3600)
+
+    openid = cache.get("openid", None)
+    nickname = cache.get("nickname", None)
+    sex = cache.get("sex", None)
+    language = cache.get("language", None)
+    city = cache.get("city", None)
+    province = cache.get("province", None)
+    country = cache.get("country", None)
+    headimgurl = cache.get("headimgurl", None)
+
+    data = {
+        'openid': openid,
+        'nickname': nickname,
+        'sex': sex,
+        'language': language,
+        'city': city,
+        'province': province,
+        'country': country,
+        'headimgurl': headimgurl
     }
     return render(request, 'index.html', data)
